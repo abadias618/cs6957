a5324532e97fae7a1665a8882e9febca9699c35e
Abadias618 <abadias618@gmail.com>
2023-09-19 22:37:14 -0600

    sep19

--- project_1/cbow.py
+++ project_1/cbow.py
@@ -15,17 +15,16 @@ class CBOW(torch.nn.Module):
         self.activation_function2 = nn.LogSoftmax(dim = -1)
 
     def forward(self, inputs):
-        embeds = sum(self.embeddings(inputs)).view(1,-1)
+        embeds = self.embeddings(inputs).mean(1).squeeze(1)#sum(self.embeddings(inputs)).view(1,-1)
         out = self.linear1(embeds)
         out = self.activation_function1(out)
         out = self.linear2(out)
         out = self.activation_function2(out)
         return out
     
-
-def get_word_emdedding(self, word):
-    word = torch.LongTensor([vocab[word]])
-    return self.embeddings(word).view(1,-1)
+    def get_word_emdedding(self, vocab, word):
+        word = torch.LongTensor([vocab[word]])
+        return self.embeddings(word).view(1,-1)
 
 def make_sentence_vector(sentence, word_dict):
     idxs = [word_dict[w] for w in sentence]
--- project_1/main.py
+++ project_1/main.py
@@ -1,49 +1,77 @@
+import os
 import sys
 sys.path.insert(0, './scripts/')
+from tqdm import tqdm
+from torch.utils.data import DataLoader, TensorDataset
 from utils import *
 from cbow import *
 
 
 vocab = get_word2ix("./vocab.txt")
-inverse_vocab = {v: k for k, v in vocab.items()}
-window = 2
-print("\nexample vocab",{k: vocab[k] for k in list(vocab)[:5]},"\n")
+print("vocab length:",len(vocab.keys()))
+WINDOW = 5
+#print("\nexample vocab",{k: vocab[k] for k in list(vocab)[:5]},"\n")
 files = get_files("./data/train")
-print("files in dir",files[0],"\n")
-vectors = process_data(files[0:5], window, vocab)
-print("data:",len(vectors[0]),len(vectors[1]),len(vectors[2]),len(vectors[3]),len(vectors[4]))
-def vec2data(vecs, window):
+#print("files in dir",files[0],"\n")
+vectors = process_data(files[0:2], WINDOW, vocab)
+print("vectors length:",len(vectors))
+#print("\ndata example lengths:",len(vectors[0]),len(vectors[1]))#,len(vectors[2]))
+def vec2data(vecs, win):
     df = []
+    labels = []
     for v in vecs:
-        for i in range(window, len(v) - window):
-            words = v[i - window:i] + v[i + 1:i + window]
-            df.append((words, v[i]))
-    return df
-data = vec2data(vectors, window)
+        for i in range(win, len(v) - win):
+            words = v[i - win:i] + v[i + 1:i + win + 1]
+            df.append(words)
+            labels.append(v[i])
+    return df, labels
+data, labels = vec2data(vectors, WINDOW)
+#print("\ndata length=",len(data))
+#print("\nexample data",data[0],"\n")
+#print("\nexample data",data[1],"\n")
+#print("\nexample data",data[2],"\n")
+
+VOCAB_SIZE = len(list(vocab.keys()))
+EMBEDDING_DIM = 100
+print(f"VOCAB_SIZE: {VOCAB_SIZE}, embedding_size: {EMBEDDING_DIM}")
 
 
-vocab_size = len(list(vocab.keys()))
-embedding_dim = 5
-print(f"vocab_size: {vocab_size}, embedding_size: {embedding_dim}")
 
 
-model = CBOW(vocab_size, embedding_dim)
-loss_function = nn.NLLLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
-print(make_sentence_vector(['stormy','nights','when','the'], vocab))
 
-#train
-for epoch in range(50):
-    epoch_loss = 0
-    for sentence, target in data:
-        model.zero_grad()
-        #sentence_vector = make_sentence_vector(sentence, vocab)  
-        sentence_vector = torch.tensor(sentence)
-        log_probs = model(sentence_vector)
-        loss = loss_function(log_probs, torch.tensor(
-        [target], dtype=torch.long))
-        loss.backward()
-        optimizer.step()
-        epoch_loss += loss.data
-    print('Epoch: '+str(epoch)+', Loss: ' + str(epoch_loss.item()))
+dataset = TensorDataset(torch.tensor(data), torch.tensor(labels))
+dataloader = DataLoader(dataset, batch_size = 64, shuffle=False)
+for lr in [0.01, 0.001, 0.0001]:
+    model = CBOW(VOCAB_SIZE, EMBEDDING_DIM)
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+    overall_loss = float('inf')
+    #train
+    for epoch in range(1):
+        with tqdm(dataloader) as tepoch:
+            
+            for sentence, target in tepoch:
+                tepoch.set_description(f"Epoch {epoch}")
+
+                model.zero_grad()
+                sentence_vector = sentence
+                log_probs = model(sentence)
+                loss = loss_function(log_probs, target)
+                loss.backward()
+                optimizer.step()
+                tepoch.set_postfix(loss=loss.item())
+            print('Epoch# '+str(epoch)+' - Loss: ' + str(loss.item()))
+            if loss < overall_loss:
+                overall_loss = loss
+                if os.path.exists("./embeddings.txt"):
+                    os.remove("embeddings.txt")
+                with open("embeddings.txt","w") as f:
+                    f.write(f"{str(VOCAB_SIZE)} {str(EMBEDDING_DIM)}\n")
+                    for w in vocab.keys():
+                        word = w
+                        embeds = model.get_word_emdedding(vocab, word)[0].tolist()
+                        embeds = " ".join(str(e) for e in embeds)
+                        f.write(f"{word} {embeds}\n")
+
+
