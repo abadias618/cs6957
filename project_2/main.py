1b62665e99566fb52ac8365c6fe4a95174a10e77
abdias <abadias618@gmail.com>
2023-10-05 00:07:00 -0600

    oct3

--- project_2/main.py
+++ project_2/main.py
@@ -85,13 +85,13 @@ def main():
         
         # UAS - LAS
         print("sanity",obj_test_data)
-        m_predictions_test = parse_n_predict(obj_test_data, tagset=tagset,
+        m_predictions_test = parse_n_predict(hidden_data=obj_test_data, tagset=tagset,
                                     c_window=C_WINDOW, glove=glove,
                                     torch_emb=torch_emb,
                                     pos_set_name2idx=pos_set_name2idx, model=model_mean,
                                     tag_set_idx2name=tag_set_idx2name, type="mean")
         print("sanity",obj_test_data)
-        c_predictions_test = parse_n_predict(obj_test_data, tagset=tagset,
+        c_predictions_test = parse_n_predict(hidden_data=obj_test_data, tagset=tagset,
                                     c_window=C_WINDOW, glove=glove,
                                     torch_emb=torch_emb,
                                     pos_set_name2idx=pos_set_name2idx, model=model_concat,
