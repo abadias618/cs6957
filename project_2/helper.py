44b1880041913803d04fb83b74d76b7c0fc547f0
abdias <abadias618@gmail.com>
2023-10-04 18:52:11 -0600

    oct3

--- project_2/helper.py
+++ project_2/helper.py
@@ -1,7 +1,8 @@
 from scripts import state
+import numpy as np
 import torch
 
-def prepare_vectors_for_training(raw_data, tagset, C_WINDOW, glove, torch_emb, pos_set_name2idx, tag_set_name2idx):
+def prepare_vectors_for_training(raw_data, tagset, c_window, glove, torch_emb, pos_set_name2idx, tag_set_name2idx):
     train_mean = []
     train_concat = []
     labels = []
@@ -23,14 +24,14 @@ def prepare_vectors_for_training(raw_data, tagset, C_WINDOW, glove, torch_emb, p
 
             #pad
             w_stack = [w.word for w in s.stack]
-            w_stack = state.pad(w_stack, C_WINDOW, "token")
+            w_stack = state.pad(w_stack, c_window, "token")
             p_stack = [p.pos for p in s.stack]
-            p_stack = state.pad(p_stack, C_WINDOW, "postag")
+            p_stack = state.pad(p_stack, c_window, "postag")
 
             w_buffer = [w.word for w in s.parse_buffer]
-            w_buffer = state.pad(w_buffer, C_WINDOW, "token")
+            w_buffer = state.pad(w_buffer, c_window, "token")
             p_buffer = [p.pos for p in s.parse_buffer]
-            p_buffer = state.pad(p_buffer, C_WINDOW, "postag")
+            p_buffer = state.pad(p_buffer, c_window, "postag")
 
             w = w_stack + w_buffer
             w_emb = glove.get_vecs_by_tokens(w, lower_case_backup=True)
@@ -58,6 +59,70 @@ def prepare_vectors_for_training(raw_data, tagset, C_WINDOW, glove, torch_emb, p
             train_concat.append(torch.add(w_emb_concat, p_emb_concat))
     return train_mean, train_concat, labels
 
-def get_vectors():
-    return
+def parse_n_predict(hidden_data, tagset, c_window, glove, torch_emb, pos_set_name2idx, model, tag_set_idx2name):
+    predictions = []
+    for row in hidden_data:
+        s = state.ParseState([],row,[])
+        deps_predicted = []
+        while not state.is_final_state(s, c_window):
+            w_stack = [w.word for w in s.stack]
+            w_stack = state.pad(w_stack, c_window, "token")
+            p_stack = [p.pos for p in s.stack]
+            p_stack = state.pad(p_stack, c_window, "postag")
+
+            w_buffer = [w.word for w in s.parse_buffer]
+            w_buffer = state.pad(w_buffer, c_window, "token")
+            p_buffer = [p.pos for p in s.parse_buffer]
+            p_buffer = state.pad(p_buffer, c_window, "postag")
+
+            w = w_stack + w_buffer
+            w_emb = glove.get_vecs_by_tokens(w, lower_case_backup=True)
+            # mean representation
+            w_emb_mean = torch.mean(w_emb, 0)
+            # concat representation
+            w_emb_concat = w_emb[0]
+            for i in range(1,len(w_emb)):
+                w_emb_concat = torch.cat((w_emb_concat, w_emb[i]),0)
+
+            
+            p = p_stack + p_buffer
+            p = [pos_set_name2idx[tag] for tag in p]
+            p_emb = torch_emb(torch.Tensor(p).to(torch.int64))
+            # mean representation
+            p_emb_mean = torch.mean(p_emb, 0)
+            # concat representation
+            p_emb_concat = p_emb[0]
+            for i in range(1,len(p_emb)):
+                p_emb_concat = torch.cat((p_emb_concat, p_emb[i]),0)
+
+            pred = model(torch.add(w_emb_mean, p_emb_mean))
+            pred_text = tag_set_idx2name[round(np.argmax(pred.data.numpy()))]
+
+            action = pred_text
+
+            if action not in tagset:
+                raise Exception()
+            #check for action validity
+            if not state.is_action_valid(s, action):
+                sorted_probs = np.sort(pred.data.numpy())[::-1]
+                for i in range(1,len(sorted_probs)): # 1 start bc we already checked argmax before
+                    if state.is_action_valid(s, tag_set_idx2name[i]):
+                        action =  tag_set_idx2name[i]
+                        break
+
+            if not state.is_action_valid(s, action):
+                break
+            a = action.split("_")
+            
+            if  len(a) > 1:
+                if a[1] == "L":
+                    state.left_arc(s, action)
+                elif a[1] == "R":   
+                    state.right_arc(s, action)
+                deps_predicted.append(action)
+            else:
+                state.shift(s)
+                deps_predicted.append(action)
+        predictions.append([deps_predicted,s.dependencies])
+    return predictions 
 
--- project_2/main.py
+++ project_2/main.py
@@ -1,5 +1,6 @@
 from scripts import dataloader
 from scripts import state
+from scripts import evaluate
 from helper import *
 from Parser import *
 import numpy as np
@@ -40,7 +41,7 @@ def main():
     torch_emb = nn.Embedding(NUMBER_OF_POSTAGS, DIM)
     
     train_mean, train_concat, labels = prepare_vectors_for_training(data, tagset=tagset,
-                                                       C_WINDOW=C_WINDOW, glove=glove,
+                                                       c_window=C_WINDOW, glove=glove,
                                                        torch_emb=torch_emb,
                                                        pos_set_name2idx=pos_set_name2idx,
                                                        tag_set_name2idx=tag_set_name2idx)
@@ -66,85 +67,42 @@ def main():
     model_mean = train_model(dataloader_mean, model_mean, loss_function, optimizer_mean, epochs=1)
     #model_concat = train_model(dataloader_concat, model_concat, loss_function, optimizer_concat, epochs=1)
     
-    pred = model_mean(train_mean[0])
-    #print("raw pred\n",pred)
-    #print("pred.data",pred.data)
-    #print("argmax",np.argmax(pred.data.numpy()))
-    print("result?",tag_set_idx2name[round(np.argmax(pred.data.numpy()))])
+    # UAS - LAS
+    test_data = dataloader.load_data("./data/test.txt")
+    obj_test_data = [] #tokens-dependencies-ParseState
+    gold_actions = []
+    word_lists = []
+    #put data into objs
+    for row in test_data[:100]:
+        tokens = \
+        [state.Token(i+1,input_token,pos_tag) for i, (input_token, pos_tag) in enumerate(zip(row[0], row[1]))]
+        obj_test_data.append(tokens)
+        word_lists.append(row[0])
+        gold_actions.append(row[2])
+    
+    predictions_test = parse_n_predict(obj_test_data, tagset=tagset,
+                                 c_window=C_WINDOW, glove=glove,
+                                 torch_emb=torch_emb,
+                                 pos_set_name2idx=pos_set_name2idx, model=model_mean,
+                                 tag_set_idx2name=tag_set_idx2name)
 
+    uas_las = evaluate.compute_metrics(word_lists, gold_actions, 
+                                       [p[0] for p in predictions_test], C_WINDOW)
+
+    print("UAS-LAS", uas_las)
+    
     hidden_data = dataloader.load_hidden("./data/hidden.txt")
-    ##print(hidden_data[:2],"\n")
     obj_hidden_data = [] #tokens-dependencies-ParseState
     #put data into objs
     for row in hidden_data[:100]:
         tokens = \
         [state.Token(i+1,input_token,pos_tag) for i, (input_token, pos_tag) in enumerate(zip(row[0], row[1]))]
         obj_hidden_data.append(tokens)
-
-    # loop
-    for row in obj_hidden_data:
-        s = state.ParseState([],row,[])
-        while not state.is_final_state(s, C_WINDOW):
-            w_stack = [w.word for w in s.stack]
-            w_stack = state.pad(w_stack, C_WINDOW, "token")
-            p_stack = [p.pos for p in s.stack]
-            p_stack = state.pad(p_stack, C_WINDOW, "postag")
-
-            w_buffer = [w.word for w in s.parse_buffer]
-            w_buffer = state.pad(w_buffer, C_WINDOW, "token")
-            p_buffer = [p.pos for p in s.parse_buffer]
-            p_buffer = state.pad(p_buffer, C_WINDOW, "postag")
-
-            w = w_stack + w_buffer
-            w_emb = glove.get_vecs_by_tokens(w, lower_case_backup=True)
-            # mean representation
-            w_emb_mean = torch.mean(w_emb, 0)
-            # concat representation
-            w_emb_concat = w_emb[0]
-            for i in range(1,len(w_emb)):
-                w_emb_concat = torch.cat((w_emb_concat, w_emb[i]),0)
-
-            
-            p = p_stack + p_buffer
-            p = [pos_set_name2idx[tag] for tag in p]
-            p_emb = torch_emb(torch.Tensor(p).to(torch.int64))
-            # mean representation
-            p_emb_mean = torch.mean(p_emb, 0)
-            # concat representation
-            p_emb_concat = p_emb[0]
-            for i in range(1,len(p_emb)):
-                p_emb_concat = torch.cat((p_emb_concat, p_emb[i]),0)
-
-            pred = model_mean(torch.add(w_emb_mean, p_emb_mean))
-            pred_text = tag_set_idx2name[round(np.argmax(pred.data.numpy()))]
-
-            action = pred_text
-
-            if action not in tagset:
-                raise Exception()
-            #check for action validity
-            if not state.is_action_valid(s, action):
-                #print("entered not valid chunk because of",action)
-
-                sorted_probs = np.sort(pred.data.numpy())[::-1]
-                for i in range(1,len(sorted_probs)): # 1 start bc we already checked argmax before
-                    if state.is_action_valid(s, tag_set_idx2name[i]):
-                        action =  tag_set_idx2name[i]
-                        break
-
-            if not state.is_action_valid(s, action):
-                print("\n\n couldn't find a viable action")
-                print("\nSo far the relations are:\n",[x.label for x in s.dependencies])
-                print("FINISHED")
-                break
-            a = action.split("_")
-            
-            if  len(a) > 1:
-                if a[1] == "L":
-                    state.left_arc(s, action)
-                elif a[1] == "R":   
-                    state.right_arc(s, action)
-            else:
-                state.shift(s)
+    predictions_hidden = parse_n_predict(obj_hidden_data, tagset=tagset,
+                                 c_window=C_WINDOW, glove=glove,
+                                 torch_emb=torch_emb,
+                                 pos_set_name2idx=pos_set_name2idx, model=model_mean,
+                                 tag_set_idx2name=tag_set_idx2name)
+    print("predictions for hidden",len(predictions_hidden))
     print("MAIN FINISHED")
 main()
