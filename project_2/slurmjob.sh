3dd990239f02c70445fa6019d4b6ab90c11a033d
abdias <abadias618@gmail.com>
2023-10-05 16:31:52 -0600

    oct5

--- project_2/slurmjob.sh
+++ project_2/slurmjob.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --account soc-gpu-np
 #SBATCH --partition soc-gpu-np
-#SBATCH --ntasks-per-node=32
-#SBATCH --nodes=1
-#SBATCH --gres=gpu:1 
-#SBATCH --time=2:00:00
+#SBATCH --ntasks-per-node=64
+#SBATCH --nodes=2
+#SBATCH --gres=gpu:2 
+#SBATCH --time=3:00:00
 #SBATCH --mem=10GB
 #SBATCH --mail-user=u1419615@umail.utah.edu
 #SBATCH --mail-type=FAIL,END
